b43029351bec1ed7c32355070f0de066799cdb90
Amet <sunowar66@gmail.com>
2025-11-10 11:14:24 +0700

    remembersaveable

--- app/src/main/java/com/samuelalfito/teamexercise2/ui/TodoScreen.kt
+++ app/src/main/java/com/samuelalfito/teamexercise2/ui/TodoScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
